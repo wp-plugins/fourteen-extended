aa593e7e58c2e4fdbd3fc82766306a617240f2a0
ZGani <ZGani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-02 20:52:05 +0000

    Reverting some changes from previous version.
    git-svn-id: https://plugins.svn.wordpress.org/fourteen-extended/tags/1.1.0@831902 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: Fourteen Extended
+Plugin URI:  http://wordpress.org/plugins/fourteen-extended
+Description: A functionality plugin for extending the Twenty Fourteen theme.
+Author:      Zulfikar Nore
+Author URI:  http://www.wpstrapcode.com/
+Version:     1.1.0
+License:     GPL
+*/
+
+// Only run if theme or parent theme is Twenty Fourteen.
+if ( get_template() != 'twentyfourteen' ) {
+	return;
+}
+
+/**
+ * Loads the plugin textdomain for translations.
+ *
+ * @since Fourteen Extended 1.0.0
+ *
+ * @return void
+ */
+function fourteenxt_extended_load_textdomain() {
+	// This will load the WordPress-downloaded language pack if it exists, as languages are not bundled with the plugin.
+	load_plugin_textdomain( 'fourteenxt' );
+}
+add_action( 'plugins_loaded', 'fourteenxt_extended_load_textdomain' );
+
+// Customizer Moved to inc folder - since v1.0.9
+require_once('inc/fourteenxt-customizer.php'); // Include Extended Customizer
+
+/**
+ * Extend the default WordPress body classes and run them according to our settings.
+ *
+ * Adds body classes Index views Full-width content layout.
+ * Single views Full-width content layout.
+ * Fully centred site
+ *
+ * @since Fourteen Extended 1.0.0
+ *
+ * @param array $classes A list of existing body class values.
+ * @return array The filtered body class list.
+ */
+function fourteenxt_body_classes( $classes ) {
+global $content_width;
+	
+	if (get_theme_mod( 'fourteenxt_fullwidth_blog_feed' ) != 0 ) {
+    if (is_home() ) : 
+        $classes[] = 'full-width';
+	endif;
+    }
+	
+	if (get_theme_mod( 'fourteenxt_fullwidth_single_post' ) != 0 ) {
+	if (is_singular() && !is_page() ) : 
+        $classes[] = 'full-width';
+   	endif;
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'fourteenxt_body_classes' );
+
+function fourteenxt_blog_feed_cat( $query ) {
+    if ( is_admin() || ! $query->is_main_query() )
+        return;
+
+    if ( is_home() ) {
+        // Display posts in ascending order on the blog feed/archive
+        $query->set( 'category_name', get_theme_mod( 'fourteenxt_feed_cat' ));
+        return;
+    }
+}
+add_action( 'pre_get_posts', 'fourteenxt_blog_feed_cat', 1 );
+
+/**
+ * Loads the fitvids plugin.
+ *
+ * @since Fourteen Extended 1.0.9
+ */
+
+// add FitVids to site
+if ( get_theme_mod( 'fourteenxt_fitvids_enable' ) != 0 ) {
+
+function fourteenxt_fitvids_scripts() {
+   	// add fitvids
+	wp_register_script( 'fourteenxt_fitvids',plugin_dir_url( __FILE__ ).'js/jquery.fitvids.js','extended-fitvids', array( 'jquery' ), true );
+    wp_enqueue_script( 'fourteenxt_fitvids' );
+	
+} // end fitvids_scripts
+add_action('wp_enqueue_scripts','fourteenxt_fitvids_scripts', 20);
+
+// selector script
+function fourteenxt_fitthem() { ?>
+   	<script type="text/javascript">
+   	jQuery(document).ready(function() {
+   		jQuery('<?php echo get_theme_mod('fourteenxt_fitvids_selector'); ?>').fitVids({ customSelector: '<?php echo stripslashes(get_theme_mod('fourteenxt_fitvids_custom_selector')); ?>'});
+   	});
+   	</script>
+<?php } // End selector script
+add_action( 'wp_footer', 'fourteenxt_fitthem', 210 );
+} // End FitVids enable
+
+// Styles Moved to inc folder - since v1.0.9
+require_once('inc/fourteenxt-styles.php'); // Include Extended Styles
---
+++ inc/fourteenxt-customizer.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Add and modify the customizer settings and controls.
+ *
+ * @since Fourteen Extended 1.0.0
+ *
+ */
+function fourteenxt_customize_register( $wp_customize ) {
+
+    $wp_customize->add_section( 'fourteenxt_general_options' , array(
+       'title'      => __('TwentyFourteen General Options','fourteenxt'),
+	   'description' => sprintf( __( 'Use the following settings to set general theme options. Current options are: Center the site, Set Blog feed to full width, Set single post to full width, set content max-width and move content to below the featured image with more to come!', 'fourteenxt' )),
+       'priority'   => 30,
+    ) );
+	
+	$wp_customize->add_section( 'fourteenxt_fitvids_options' , array(
+       'title'      => __('TwentyFourteen FitVids Options','fourteenxt'),
+	   'description' => sprintf( __( 'Use the following settings to set fitvids script options. Options are: Enable script, Set selector (Default is .post) and set custom selector (optional) for other areas like .sidebar or a custom section!', 'fourteenxt' )),
+       'priority'   => 31,
+    ) );
+	
+	// Lets center the site shall we
+    $wp_customize->add_setting(
+        'fourteenxt_center_site'
+    );
+
+    $wp_customize->add_control(
+        'fourteenxt_center_site',
+    array(
+        'type'     => 'checkbox',
+        'label'    => __('Center Align Site Layout?', 'fourteenxt'),
+        'section'  => 'fourteenxt_general_options',
+		'priority' => 1,
+        )
+    );
+	
+	// Primary position switch - float it to the left.
+	$wp_customize->add_setting( 'fourteenxt_primary_menu_float', array(
+		'default' => 'right',
+	) );
+	
+	$wp_customize->add_control( 'fourteenxt_primary_menu_float', array(
+    'label'   => __( 'Float Primary Menu to the left?', 'fourteenxt' ),
+    'section' => 'fourteenxt_general_options',
+	'priority' => 2,
+    'type'    => 'radio',
+        'choices' => array(
+            'right' => __( 'Default - Right', 'fourteenxt' ),
+            'left'  => __( 'Float Left', 'fourteenxt' ),
+        ),
+    ));
+	
+	// Set Blog feed to full width i.e. hide the content sidebar.
+	$wp_customize->add_setting(
+        'fourteenxt_fullwidth_blog_feed'
+    );
+
+    $wp_customize->add_control(
+        'fourteenxt_fullwidth_blog_feed',
+    array(
+        'type'     => 'checkbox',
+        'label'    => __('Check to hide the sidebar on the blog feed', 'fourteenxt'),
+        'section'  => 'fourteenxt_general_options',
+		'priority' => 3,
+        )
+    );
+	
+	// Set Single to full width i.e. hide the content sidebar.
+	$wp_customize->add_setting(
+        'fourteenxt_fullwidth_single_post'
+    );
+
+    $wp_customize->add_control(
+        'fourteenxt_fullwidth_single_post',
+    array(
+        'type'     => 'checkbox',
+        'label'    => __('Check to show full width single post', 'fourteenxt'),
+        'section'  => 'fourteenxt_general_options',
+		'priority' => 4,
+        )
+    );
+	
+	$wp_customize->add_setting(
+    'fourteenxt_content_off_featured_image',
+    array(
+        'default' => '',
+    ));
+	
+	$wp_customize->add_control(
+    'fourteenxt_content_off_featured_image',
+    array(
+        'label' => __('Enter 1 or more for padding to move content off featured image (numbers only!) - default is -48','fourteenxt'),
+        'section' => 'fourteenxt_general_options',
+		'priority' => 5,
+        'type' => 'text',
+    ));
+	
+	$wp_customize->add_setting(
+    'fourteenxt_content_width_adjustment',
+    array(
+        'default' => '474',
+    ));
+	
+	$wp_customize->add_control(
+    'fourteenxt_content_width_adjustment',
+    array(
+        'label' => __('Set Content max-width (numbers only!) - maximum recommended is 874 & Default is 474','fourteenxt'),
+        'section' => 'fourteenxt_general_options',
+		'priority' => 6,
+        'type' => 'text',
+    ));
+	
+	// Blog Feed Category Selector
+    $categories = get_categories();
+	$cats = array( 'All Categories' );
+	$i = 0;
+	foreach($categories as $category){
+		if($i==0){
+			$default = $category->slug;
+			$i++;
+		}
+		$cats[$category->slug] = $category->name;
+	}
+ 
+	$wp_customize->add_setting('fourteenxt_feed_cat', array(
+		'default'  => '',
+	));
+	$wp_customize->add_control( 'fourteenxt_feed_cat', array(
+		'settings' => 'fourteenxt_feed_cat',
+		'label'   => __('Select Blog Feed Category:', 'fourteenxt'),
+		'section'  => 'fourteenxt_general_options',
+		'priority' => 8,
+		'type'    => 'select',
+		'choices' => $cats,
+	));
+	
+	// Add FitVids to site
+    $wp_customize->add_setting(
+        'fourteenxt_fitvids_enable'
+    );
+
+    $wp_customize->add_control(
+        'fourteenxt_fitvids_enable',
+    array(
+        'type'     => 'checkbox',
+        'label'    => __('Enable FitVids?', 'fourteenxt'),
+        'section'  => 'fourteenxt_fitvids_options',
+		'priority' => 1,
+        )
+    );
+	
+	$wp_customize->add_setting(
+    'fourteenxt_fitvids_selector',
+    array(
+        'default' => '.post',
+    ));
+	
+	$wp_customize->add_control(
+    'fourteenxt_fitvids_selector',
+    array(
+        'label' => __('Enter a selector for FitVids - i.e. .post','fourteenxt'),
+        'section' => 'fourteenxt_fitvids_options',
+		'priority' => 2,
+        'type' => 'text',
+    ));
+	
+	$wp_customize->add_setting(
+    'fourteenxt_fitvids_custom_selector',
+    array(
+        'default' => '',
+    ));
+	
+	$wp_customize->add_control(
+    'fourteenxt_fitvids_custom_selector',
+    array(
+        'label' => __('Enter a custom selector for FitVids - i.e. .sidebar','fourteenxt'),
+        'section' => 'fourteenxt_fitvids_options',
+		'priority' => 3,
+        'type' => 'text',
+    ));
+}
+add_action( 'customize_register', 'fourteenxt_customize_register' );
