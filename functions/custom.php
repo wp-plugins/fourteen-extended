1a6db4c2effdbf4a8dc81a747335545bcf52d24c
ZGani <ZGani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-07 04:22:26 +0000

    Updated to the latest version Custom Meta Boxes
    git-svn-id: https://plugins.svn.wordpress.org/fourteen-extended/tags/1.2.33@1084298 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions/custom.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Include and setup custom metaboxes and fields. (make sure you copy this file to outside the CMB directory)
+ *
+ * @category YourThemeOrPlugin
+ * @package  Metaboxes
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     https://github.com/webdevstudios/Custom-Metaboxes-and-Fields-for-WordPress
+ */
+
+/**
+ * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
+ */
+
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+
+/**
+ * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
+ *
+ * @param  CMB2_Field object $field Field object
+ *
+ * @return bool                     True if metabox should show
+ */
+function cmb2_hide_if_no_cats( $field ) {
+	// Don't show this field if not in the cats category
+	if ( ! has_tag( 'cats', $field->object_id ) ) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Conditionally displays a message if the $post_id is 2
+ *
+ * @param  array             $field_args Array of field parameters
+ * @param  CMB2_Field object $field      Field object
+ */
+function cmb2_before_row_if_2( $field_args, $field ) {
+	if ( 2 == $field->object_id ) {
+		echo '<p>Testing <b>"before_row"</b> parameter (on $post_id 2)</p>';
+	} else {
+		echo '<p>Testing <b>"before_row"</b> parameter (<b>NOT</b> on $post_id 2)</p>';
+	}
+}
+
+add_filter( 'cmb2_meta_boxes', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ *
+ * @param  array $meta_boxes
+ * @return array
+ */
+function cmb2_sample_metaboxes( array $meta_boxes ) {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_fourteenxt_';
+
+	/**
+	 * Sample metabox to demonstrate each field type included
+	 */
+	$meta_boxes['enable_true_fullwidth'] = array(
+		'id'            => 'enable_true_fullwidth',
+		'title'         => __( 'Fourteen Extended Page Control', 'cmb2' ),
+		'object_types'  => array( 'page', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // true to keep the metabox closed by default
+		'fields'        => array(			
+			array(
+				'name' => __( 'Set Page To True Full-width?', 'cmb2' ),
+				'desc' => __( 'Check this box to make this page a true full width - this will remove the left sidebar from the page. You must have the full-width template selected for this to work!', 'cmb2' ),
+				'id'   => $prefix . 'true_fullwidth',
+				'type' => 'checkbox',
+			),			
+		),
+	);	
+
+	// Add other metaboxes as needed
+
+	return $meta_boxes;
+}
